54540060e4748581459c589b4a4d5152dba09c56
Daniel Toffetti <dtoffe@gmail.com>
2023-03-23 19:55:32 -0300

    Start testing parser

--- src/dpl0c.d
+++ src/dpl0c.d
@@ -1,8 +1,10 @@
 module dpl0c;
 
 import std.stdio;
+import ast;
 import error;
 import lexer;
+import parser;
 import token;
 
 enum VERSION = "0.0.1";
@@ -10,7 +12,7 @@ enum VERSION = "0.0.1";
 void main(string[] args) {
     writefln("\nThe D PL/0 Compiler v. %s", VERSION);
 
-    string sourceFileName = "../examples/square.pl0";
+    string sourceFileName = "../examples/test.pl0";
     Lexer lex = new Lexer(sourceFileName);
     Token tok = lex.nextToken();
     while (tok.getTokenType()  != TokenType.EOF) {
@@ -18,5 +20,7 @@ void main(string[] args) {
             " Line: ", tok.getLine(), " Column: ", tok.getColumn());
         tok = lex.nextToken();
     }
+    Parser parser = new Parser(lex);
+    ProgramNode node = parser.parseProgram();
     ErrorManager.printErrors();
 }
