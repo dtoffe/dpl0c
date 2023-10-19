c01177ac38f4bf4adb62fd9b278359184451f4d7
Daniel Toffetti <dtoffe@gmail.com>
2023-10-19 20:24:06 -0300

    Replaced old read and write, those were intended for file operations and they should have never been commited in the first place

--- src/runtime/dpl0crt.c
+++ src/runtime/dpl0crt.c
@@ -1,43 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/** 
- * call runtime like this: call void @write(i32 %fd, i8* %buf, i32 %count)
- */
-int write(int fd, char *buf, int count) {
-  // Check if the file descriptor is valid.
-  if (fd < 0) {
-    return -1;
-  }
-
-  // Write the specified number of bytes to the file descriptor.
-  int bytes_written = write(fd, buf, count);
-
-  // Check if the write was successful.
-  if (bytes_written < 0) {
-    return -1;
-  }
-
-  // Return the number of bytes written.
-  return bytes_written;
+int readInteger() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    return num;
 }
 
-/** 
- * call runtime like this: call i32 @read(i32 %fd, i8* %buf, i32 %count)
- */
-int read(int fd, char *buf, int count) {
-  // Check if the file descriptor is valid.
-  if (fd < 0) {
-    return -1;
-  }
-
-  // Read the specified number of bytes from the file descriptor.
-  int bytes_read = read(fd, buf, count);
-
-  // Check if the read was successful.
-  if (bytes_read < 0) {
-    return -1;
-  }
-
-  // Return the number of bytes read.
-  return bytes_read;
+void writeInteger(int num) {
+    printf("Result: %d\n", num);
 }
